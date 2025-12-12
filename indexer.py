746fd1c0c96a824050e6f3d4ce4d2ac9aa1b2f63
saroj0090 <Sarojkumar95566@gmail.com>
2025-12-12 18:33:17 +0530

    Update indexer.py

--- indexer.py
+++ indexer.py
@@ -1,65 +1,33 @@
 #credit-@MADDY00error
 #don't remove credit
 
-from pyrogram import Client, filters
-from pyrogram.errors import FloodWait
-import asyncio
-import pymongo
 import os
+from pyrogram import Client, filters
 
 API_ID = int(os.getenv("28057612"))
 API_HASH = os.getenv("95295dca0ca9f41855ec0cabe9b440ed")
 BOT_TOKEN = os.getenv("7715353540:AAG9tE3FFxy2d8mbcmYn-CNgukwTPPafs3A")
 
-#Optional
-MONGO_URI = os.getenv("MONGO_URI")
-SOURCE_CHANNELS = list(map(int, os.getenv("-1002435736305").split()))
+# space-separated list of source channels  
+SOURCE_CHANNELS = [int(x) for x in os.getenv("-1002435736305").split()]
 TARGET_CHANNEL = int(os.getenv("-1002636535986"))
 
-client = pymongo.MongoClient(MONGO_URI)
-db = client["AutoIndexer"]
-collection = db["Indexed"]
-
-bot = Client(
-    "AutoIndexerBot",
+app = Client(
+    "forward-bot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN
 )
 
-@bot.on_message(filters.channel & filters.chat(SOURCE_CHANNELS))
-async def index_files(client, message):
-
-    if not message.media:
-        return
-
-    file_id = message.id  
-    chat_id = message.chat.id  
-
-    # Prevent duplicate indexing
-    exists = collection.find_one({"chat_id": chat_id, "file_id": file_id})
-    if exists:
-        return
-
+@app.on_message(filters.chat(SOURCE_CHANNELS))
+async def forward_message(client, message):
     try:
-        forwarded = await message.copy(TARGET_CHANNEL)
+        await message.copy(TARGET_CHANNEL)
+        print(f"Forwarded from {message.chat.id}")
+    except Exception as e:
+        print(f"Error: {e}")
 
-        # Save in Mongo
-        collection.insert_one({
-            "chat_id": chat_id,
-            "file_id": file_id,
-            "new_message_id": forwarded.id
-        })
+print("Bot is running...")
+app.run()
 
-        print(f"Indexed: {chat_id}/{file_id}")
-
-    except FloodWait as e:
-        print(f"Waiting {e.value} seconds due to flood control...")
-        await asyncio.sleep(e.value)
-    except Exception as ex:
-        print(f"Error: {ex}")
-
-
-print("🤖 Auto Indexer Running...")
-bot.run()
 
